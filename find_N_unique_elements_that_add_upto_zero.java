c223bf7375261b08088bba141e042766b0b1e0cf
2580shreyali-byte <2580shreyali@gmail.com>
2026-06-21 22:03:26 +0530

    Create find_N_unique_integers_that_add_upto_zero.java
    Leetcode 1304

---
+++ find_N_unique_elements_that_add_upto_zero.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int arr[]=new int[n];
+        if(n%2==0){
+            int j=0;
+            for(int i= -n/2;i<=n/2;i++){
+                if(i==0) continue;
+                arr[j]=i;
+                j++;
+            }
+        }
+        else{
+            int j=0;
+            for(int i= -n/2;i<=n/2;i++){
+                arr[j]=i;
+                j++;
+            }
+        }
+        return arr;
+    }
+}
