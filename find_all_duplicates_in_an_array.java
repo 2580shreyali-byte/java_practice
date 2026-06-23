3806e81665fa85467f51b4290a4a2714cb75b7f4
2580shreyali-byte <2580shreyali@gmail.com>
2026-06-23 21:15:27 +0530

    Create find_all_duplicates_in_an_array.java
    Leetcode 442

---
+++ find_all_duplicates_in_an_array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer>list=new ArrayList<>();
+        int n=nums.length;
+        int freq[]=new int[n+1];
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(freq[i]>1) list.add(i);
+        }
+        return list;
+    }
+}
