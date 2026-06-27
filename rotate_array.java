179bad57a106852478be6bfbd64fbe56025ce0d4
2580shreyali-byte <2580shreyali@gmail.com>
2026-06-27 21:04:54 +0530

    Create rotate_array.java

---
+++ rotate_array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k=k%nums.length;
+        for(int i=0,j=nums.length-k-1;i<=j;i++,j--){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+        for(int i=nums.length-k,j=nums.length-1;i<=j;i++,j--){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+        for(int i=0,j=nums.length-1;i<=j;i++,j--){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+    }
+}
