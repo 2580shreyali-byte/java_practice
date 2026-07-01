df161ab27a1cf473411272e709ff7f9488648102
2580shreyali-byte <2580shreyali@gmail.com>
2026-07-01 23:12:16 +0530

    Create palindrome_number.java
    Leetcode 9

---
+++ palindrome_number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+       int org = x;
+       int rev=0;
+       while(x!=0){
+       int temp = x%10;
+        rev=rev*10+temp;
+        x/=10; 
+        }
+        if(rev==org) return true;
+        else return false;
+    }
+}
